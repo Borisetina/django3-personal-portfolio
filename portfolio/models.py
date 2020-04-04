8d6ee6419209bb021770d231532e395f121dc433
MareGraphics <mare.sampbn@gmail.com>
2020-04-04 22:00:37 +0200

    j

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     description = models.CharField(max_length=250)
     image= models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
+    date = models.CharField(max_length=100,default='2020')
 
     def __str__(self):
         return self.title
