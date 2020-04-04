627f0f1ca056f7fea090478aa64777f986bf9ed1
MareGraphics <mare.sampbn@gmail.com>
2020-04-04 19:36:37 +0200

    asda

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_fw@azu(gn$i2rbi+57@lw+(og@osa3-bs(0leo&$k(_vjdz#0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['borisro.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
